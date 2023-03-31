f1b4642a672d3a7ed7ff126cdb3b09ebd775a1fb
TheShenk <udod_maksim@mail.ru>
2023-03-31 14:23:19 +0300

    feat: add boost & base program options

--- main.cpp
+++ main.cpp
@@ -3,7 +3,23 @@
 //
 
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+int main(int argc, char *argv[]) {
+
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "Produce help message")
+            ("max-clients", boost::program_options::value<int>(), "Set maximum allowed clients number")
+            ;
+
+    boost::program_options::variables_map vm;
+    auto parsed_options = boost::program_options::parse_command_line(argc, argv, desc);
+    boost::program_options::store(parsed_options, vm);
+    boost::program_options::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
 }
